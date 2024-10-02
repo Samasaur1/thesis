fa6ca4331a92364edbee603ae5faccca5a113eea
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-10-02 15:47:49 -0700

    app: init

---
+++ TorrentExplorer/TorrentExplorer/TorrentExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorrentExplorerApp.swift
+//  TorrentExplorer
+//
+//  Created by Sam Gauck on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TorrentExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TorrentExplorer/TorrentExplorerTests/TorrentExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TorrentExplorerTests.swift
+//  TorrentExplorerTests
+//
+//  Created by Sam Gauck on 10/2/24.
+//
+
+import Testing
+@testable import TorrentExplorer
+
+struct TorrentExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
