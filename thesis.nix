2feb3d2c479e32569274b06098d26d026c1e6e83
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-12-09 10:58:23 -0800

    check output of date

--- thesis.nix
+++ thesis.nix
@@ -4,4 +4,5 @@ runCommandNoCC "output" {} ''
   echo "This is my thesis" > $out
   echo "It was built at ${date}" >> $out
   echo "off of commit ${rev}" >> $out
+  echo "Running 'date' in the Nix build produces $(date)" >> $out
 ''
