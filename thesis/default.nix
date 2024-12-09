ddb9528adcc8c527ebb2645969fe975810460eed
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-12-09 12:22:40 -0800

    use reed thesis template

--- thesis/default.nix
+++ thesis/default.nix
@@ -1,5 +1,10 @@
 { rev, date, pandoc, getExe, runCommandNoCC, ... }:
 
 runCommandNoCC "thesis.pdf" {} ''
-  ${getExe pandoc} --defaults ${./options.yaml} --metadata-file ${./metadata.yaml} -M rev=${rev} -M date=${date} ${./chapters}/*.md -o $out
+  ${getExe pandoc} \
+    --defaults ${./options.yaml} \
+    --metadata-file ${./metadata.yaml} \
+    -M rev=${rev} -M date=${date} \
+    --template ${./reedthesis/template.tex}
+    ${./chapters}/*.md -o $out
 ''
