fe87c2564ad1dfe6cd07c6350577b32257858902
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-12-09 21:45:44 -0800

    wrap in runCommand so that result works

--- thesis/default.nix
+++ thesis/default.nix
@@ -6,25 +6,32 @@
   ...
 }:
 
-pkgs.stdenv.mkDerivation {
-  pname = "thesis";
-  version = "0.1.0";
+let
+  thesis = pkgs.stdenv.mkDerivation {
+    pname = "thesis";
+    version = "0.1.0";
 
-  src = ./.;
+    src = ./.;
 
-  buildInputs = [
-    pkgs.texliveFull
-    pkgs.pandoc
-  ];
+    buildInputs = [
+      pkgs.texliveFull
+      pkgs.pandoc
+    ];
 
-  buildPhase = ''
-    DATE="$(TZ='America/Los_Angeles' ${pkgs.lib.getExe' pkgs.coreutils "date"} -d '@${toString lastModified}' +'%B %-d, %Y at %-I:%M%P (%z)')"
+    buildPhase = ''
+      DATE="$(TZ='America/Los_Angeles' ${pkgs.lib.getExe' pkgs.coreutils "date"} -d '@${toString lastModified}' +'%B %-d, %Y at %-I:%M%P (%z)')"
 
-    pandoc \
-      --defaults options.yaml \
-      --metadata-file metadata.yaml \
-      -M commitRev=${rev} -M commitShortRev=${shortRev} -M "commitDate=$DATE" \
-      --template template.tex \
-      chapters/*.md -o $out
-  '';
-}
+      mkdir $out/doc
+
+      pandoc \
+        --defaults options.yaml \
+        --metadata-file metadata.yaml \
+        -M commitRev=${rev} -M commitShortRev=${shortRev} -M "commitDate=$DATE" \
+        --template template.tex \
+        chapters/*.md -o $out/doc/thesis.pdf
+    '';
+  };
+in
+  pkgs.runCommandNoCC "thesis.pdf" {} ''
+    ln -s ${thesis}/doc/thesis.pdf $out
+  ''
